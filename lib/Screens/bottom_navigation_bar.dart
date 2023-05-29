b49477bb42dc2623e73b06f6adfe6df1403de0f4
logoutbye <muhammadbilal03478@gmail.com>
2023-05-29 06:13:39 -0700

    navigation dynamic handled

--- lib/Screens/bottom_navigation_bar.dart
+++ lib/Screens/bottom_navigation_bar.dart
@@ -8,9 +8,26 @@ import '../main.dart';
 class FirstScreen extends StatefulWidget {
   bool isInternetConnected;
 
+  String? button1link;
+  String? button1label;
+  String? button2link;
+  String? button2label;
+  String? button3link;
+  String? button3label;
+  String? button4link;
+  String? button4label;
+
   FirstScreen({
     super.key,
     required this.isInternetConnected,
+    required this.button1label,
+    required this.button1link,
+    required this.button2label,
+    required this.button2link,
+    required this.button3label,
+    required this.button3link,
+    required this.button4label,
+    required this.button4link,
   });
 
   @override
@@ -18,9 +35,30 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+  String? _button1link;
+  String? _button1label;
+  String? _button2link;
+  String? _button2label;
+  String? _button3link;
+  String? _button3label;
+  String? _button4link;
+  String? _button4label;
+
   var IsInternetConnected = true;
   @override
   void initState() {
+    _button1label = widget.button1label;
+    _button1link = widget.button1link;
+
+    _button2label = widget.button2label;
+    _button2link = widget.button2link;
+
+    _button3label = widget.button3label;
+    _button3link = widget.button3link;
+
+    _button4label = widget.button4label;
+    _button4link = widget.button4link;
+
     fetchData();
     // TODO: implement initState
     super.initState();
@@ -33,8 +71,7 @@ class _FirstScreenState extends State<FirstScreen> {
     Home(isInternetConnected: isInternetConnected),
     Home(isInternetConnected: isInternetConnected)
   ];
-
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +98,7 @@ class _FirstScreenState extends State<FirstScreen> {
                     Icons.home_filled,
                     color: MyColors.kprimaryshade,
                   ),
-                  label: "Home"),
+                  label: '$_button1label'),
               NavigationDestination(
                   icon: Icon(
                     Icons.chat_bubble_outline,
@@ -71,7 +108,7 @@ class _FirstScreenState extends State<FirstScreen> {
                     Icons.chat_bubble_outlined,
                     color: MyColors.kprimaryshade,
                   ),
-                  label: "Blog"),
+                  label: '$_button2label'),
               NavigationDestination(
                   icon: Icon(
                     Icons.info_outline,
@@ -81,7 +118,7 @@ class _FirstScreenState extends State<FirstScreen> {
                     Icons.info,
                     color: MyColors.kprimaryshade,
                   ),
-                  label: "About Us"),
+                  label: "$_button3label"),
               NavigationDestination(
                   icon: Icon(
                     Icons.contact_mail_outlined,
@@ -91,62 +128,76 @@ class _FirstScreenState extends State<FirstScreen> {
                     Icons.contact_mail_rounded,
                     color: MyColors.kprimaryshade,
                   ),
-                  label: "Contact Us"),
+                  label: "$_button4label"),
             ]),
       ),
     );
   }
 
-  Future<ButtonData> getButtonData(String documentId) async {
-    try {
-      DocumentSnapshot documentSnapshot =
-          await _firestore.collection('App Urls').doc(documentId).get();
+  // Future<ButtonData> getButtonData(String documentId) async {
+  //   try {
+  //     DocumentSnapshot documentSnapshot =
+  //         await _firestore.collection('App Urls').doc(documentId).get();
 
-      var data = documentSnapshot.data() as Map<String, dynamic>;
+  //     var data = documentSnapshot.data() as Map<String, dynamic>;
 
-      // Create a new instance of ButtonData using the data from the document
-      ButtonData buttonData = ButtonData(
-        link: data['url'] as String,
-        label: data['label'] as String,
-      );
+  //     // Create a new instance of ButtonData using the data from the document
+  //     ButtonData buttonData = ButtonData(
+  //       link: data['url'] as String,
+  //       label: data['label'] as String,
+  //     );
 
-      return buttonData;
-    } catch (e) {
-      // Handle any errors that occur during the process
-      print('Error: $e');
-      return ButtonData(link: '', label: '');
-    }
-  }
+  //     return buttonData;
+  //   } catch (e) {
+  //     // Handle any errors that occur during the process
+  //     print('Error: $e');
+  //     return ButtonData(link: '', label: '');
+  //   }
+  // }
 
-  Future<void> fetchData() async {
-    try {
-      ButtonData button1Data = await getButtonData('Button1');
-      ButtonData button2Data = await getButtonData('Button2');
-      ButtonData button3Data = await getButtonData('Button3');
-      ButtonData button4Data = await getButtonData('Button4');
+  // Future<void> fetchData() async {
+  //   try {
+  //     ButtonData button1Data = await getButtonData('Button1');
+  //     ButtonData button2Data = await getButtonData('Button2');
+  //     ButtonData button3Data = await getButtonData('Button3');
+  //     ButtonData button4Data = await getButtonData('Button4');
 
-      // Do something with the retrieved data
-      print('Button 1 Label: ${button1Data.label}');
-      print('Button 1 Link: ${button1Data.link}');
+  //     _button1label = button1Data.label;
+  //     _button1link = button1Data.link;
 
-      print('Button 2 Label: ${button2Data.label}');
-      print('Button 2 Link: ${button2Data.link}');
+  //     _button2label = button2Data.label;
+  //     _button2link = button2Data.link;
 
-      print('Button 2 Label: ${button3Data.label}');
-      print('Button 2 Link: ${button3Data.link}');
-      
-      print('Button 2 Label: ${button4Data.label}');
-      print('Button 2 Link: ${button4Data.link}');
-    } catch (e) {
-      // Handle any errors that occur during the process
-      print('Error: $e');
-    }
-  }
+  //     _button3label = button3Data.label;
+  //     _button3link = button3Data.link;
+
+  //     _button4label = button4Data.label;
+  //     _button4link = button4Data.link;
+
+  //     // Do something with the retrieved data
+  //     print('New Button 1 Label: ${_button1label}');
+  //     print('Button 1 Label: ${button1Data.label}');
+  //     print('Button 1 Link: ${button1Data.link}');
+
+  //     print('Button 2 Label: ${button2Data.label}');
+  //     print('Button 2 Link: ${button2Data.link}');
+
+  //     print('Button 2 Label: ${button3Data.label}');
+  //     print('Button 2 Link: ${button3Data.link}');
+
+  //     print('Button 2 Label: ${button4Data.label}');
+  //     print('Button 2 Link: ${button4Data.link}');
+  //   } catch (e) {
+  //     // Handle any errors that occur during the process
+  //     print('Error: $e');
+  //   }
+  // }
+
 }
 
 class ButtonData {
-  final String link;
-  final String label;
+  String link;
+  String label;
 
   ButtonData({required this.link, required this.label});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ezeehome_webview/Contrlller/InternetConnectivity.dart';
 import 'package:ezeehome_webview/Screens/bottom_navigation_bar.dart';
 import 'package:ezeehome_webview/constants.dart';
@@ -13,11 +14,11 @@ void main() async {
 
   await checkInternetConnectionForDashboard();
 
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: MyColors.kprimaryshade,
     statusBarColor: MyColors.kprimaryshade,
   ));
-
+  await fetchData();
   runApp(MyApp());
 }
 
@@ -33,6 +34,75 @@ checkInternetConnectionForDashboard() async {
   }
 }
 
+String? _button1link;
+String? _button1label;
+String? _button2link;
+String? _button2label;
+String? _button3link;
+String? _button3label;
+String? _button4link;
+String? _button4label;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+Future<ButtonData> getButtonData(String documentId) async {
+  try {
+    DocumentSnapshot documentSnapshot =
+        await _firestore.collection('App Urls').doc(documentId).get();
+
+    var data = documentSnapshot.data() as Map<String, dynamic>;
+
+    // Create a new instance of ButtonData using the data from the document
+    ButtonData buttonData = ButtonData(
+      link: data['url'] as String,
+      label: data['label'] as String,
+    );
+
+    return buttonData;
+  } catch (e) {
+    // Handle any errors that occur during the process
+    print('Error: $e');
+    return ButtonData(link: '', label: '');
+  }
+}
+
+Future<void> fetchData() async {
+  try {
+    ButtonData button1Data = await getButtonData('Button1');
+    ButtonData button2Data = await getButtonData('Button2');
+    ButtonData button3Data = await getButtonData('Button3');
+    ButtonData button4Data = await getButtonData('Button4');
+
+    _button1label = button1Data.label;
+    _button1link = button1Data.link;
+
+    _button2label = button2Data.label;
+    _button2link = button2Data.link;
+
+    _button3label = button3Data.label;
+    _button3link = button3Data.link;
+
+    _button4label = button4Data.label;
+    _button4link = button4Data.link;
+
+    // Do something with the retrieved data
+    print('New Button 1 Label: ${_button1label}');
+    print('Button 1 Label: ${button1Data.label}');
+    print('Button 1 Link: ${button1Data.link}');
+
+    print('Button 2 Label: ${button2Data.label}');
+    print('Button 2 Link: ${button2Data.link}');
+
+    print('Button 2 Label: ${button3Data.label}');
+    print('Button 2 Link: ${button3Data.link}');
+
+    print('Button 2 Label: ${button4Data.label}');
+    print('Button 2 Link: ${button4Data.link}');
+  } catch (e) {
+    // Handle any errors that occur during the process
+    print('Error: $e');
+  }
+}
+
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
@@ -45,7 +115,17 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.pink,
         ),
-        home: FirstScreen(isInternetConnected: isInternetConnected)
+        home: FirstScreen(
+          isInternetConnected: isInternetConnected,
+          button1label: _button1label,
+          button1link: _button1link,
+          button2label: _button2label,
+          button2link: _button2link,
+          button3label: _button3label,
+          button3link: _button3link,
+          button4label: _button4label,
+          button4link: _button4link,
+        )
 
         //  Home(
         //   isInternetConnected: isInternetConnected,
